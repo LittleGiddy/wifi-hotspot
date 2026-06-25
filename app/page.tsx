c8b2f63afbba3ad61185619d2f4208823bd1bcaa
LittleGiddy <gideonfelixy@gmail.com>
2026-06-25 19:22:11 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import { prisma } from '@/lib/prisma'
 import { Package } from '@prisma/client'
 import PackageList from '@/app/components/PackageList'
+import { Suspense } from 'react'
 
 export default async function Home() {
   const packages: Package[] = await prisma.package.findMany()
-  return <PackageList packages={packages} />
+  return (
+    <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Inapakia...</div>}>
+      <PackageList packages={packages} />
+    </Suspense>
+  )
 }
